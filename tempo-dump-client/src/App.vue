b41d58611998e762e89daf45671760b0ab02bb08
Netopya <netopya@netopyaplanet.com>
2020-06-13 18:56:40 -0400

    Switched spaces to tab indentation, added tempo key saving

--- tempo-dump-client/src/App.vue
+++ tempo-dump-client/src/App.vue
@@ -1,32 +1,60 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <h1>Welcome to Tempo Dump</h1>
-    </div>
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  </div>
+	<div id="app">
+		<div class="container text-center mt-3">
+			<h1>Welcome to Tempo Dump</h1>
+		</div>
+		<KeyStep v-model="tempoKeyEdit" v-if="!tempoKey"/>
+		<!-- <img alt="Vue logo" src="./assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/> -->
+	</div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
+import KeyStep from './components/KeyStep.vue'
 
 export default {
-  name: 'App',
-  components: {
-   // HelloWorld
-  }
+	name: 'App',
+	components: {
+		// HelloWorld
+		KeyStep
+	},
+	data() {
+		return {
+			tempoKey: ''
+		};
+	},
+	computed: {
+		tempoKeyEdit: {
+			get() {
+				return this.tempoKey;
+			},
+			set(value) {
+				if (!value) return;
+
+				try {
+					localStorage.setItem('tempoKey', value);
+					this.tempoKey = value;
+				} catch(exception) {
+					alert('Failed to save tempo key');
+				}
+			}
+		}
+	},
+	created() {
+		this.tempoKey = localStorage.getItem('tempoKey');
+	}
 }
 </script>
 
 <style>
 
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; */
+	/* font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px; */
 }
 </style>
---
+++ tempo-dump-client/src/components/KeyStep.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="container mt-5">
+		<div class="row justify-content-center">
+			<form class="col-6 text-center" @submit.prevent="save">
+				<input type="text" class="form-control mb-3" placeholder="Tempo Key" :value="value" @input="tempoKey = $event.target.value"/>
+				<button class="btn btn-success btn-lg">Save</button>
+			</form>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'KeyStep',
+	data() {
+		return {
+			tempoKey: ''
+		}
+	},
+	props: {
+		value: String
+	},
+	methods: {
+		save() {
+			this.$emit('input', this.tempoKey)
+		}
+	}
+}
+</script>
