95a4369e7ea8345b73eedfb57b8850412b894cc8
Netopya <netopya@netopyaplanet.com>
2020-06-13 23:09:30 -0400

    Prevent pinging tempo if auth is empty

--- server/work-attributes.php
+++ server/work-attributes.php
@@ -6,6 +6,10 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 $auth_key = $_POST["auth_key"];
 
+if (empty($auth_key)) {
+	die();
+}
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.tempo.io/core/3/work-attributes");
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
